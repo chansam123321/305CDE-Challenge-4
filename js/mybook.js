a518f7dc13fb389dec103a2d3badd0443f1b756e
chansam123321 <lokkwan123@gmail.com>
2017-02-28 15:06:03 +0800

    Add files via upload

--- js/mybook.js
+++ js/mybook.js
@@ -1,15 +1,15 @@
-document.getElementById('Query').onkeypress = searchPrep;
+document.getElementById('InputTest').onkeypress = searchPrep;
 
 function searchPrep() {
 	console.log('search');
-	var text = document.getElementById('Query').value;
-	if (text.length > 2) {
+	var Input = document.getElementById('InputTest').value;
+	if (Input.length > 2) {
 		console.log('start search');
-		search(text);
+		search(Input);
 	}
 }
-function search(text) {
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ text +"&format=json&callback=?";
+function search(Input) {
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ Input +"&format=json&callback=?";
        $.ajax({
      url: url,
      type: 'GET',
@@ -18,48 +18,11 @@ function search(text) {
          dataType: "json",
          success: function(data, status, jqXHR) {
            console.log(data);
-           $("#Suggestions”").html();
+           $("#OutputDiv").html();
            for(var i=0;i<data[1].length;i++){
-             $("#Suggestions”").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+             $("#OutputDiv").prepend("<div><div class='Output'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
            }
 
          }
    })
 }
-/*
-function search(text) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+text+"&format=json&callback=?", true);
-	xhr.onload = function (e) {
-		if (xhr.readyState === 4) {
-			if (xhr.status === 200) {
-				//console.log(xhr.responseText);
-        /*
-				var data = JSON.parse(xhr.responseText);
-				//console.log(data.totalItems);
-				var books = data.items;
-				//var booklist = [];
-				var table = document.getElementById('searchResults');
-				table.innerHTML = '';
-				for(var i=0; i<books.length; i++) {
-					//booklist.push({title: books[i].volumeInfo.title});
-					var row = document.createElement('tr');
-					row.innerHTML = '<td>'+books[i].volumeInfo.title+'</td>';
-					//console.log(books[i].id);
-					//row.onclick = showDetails(books[i].id);
-					table.appendChild(row);
-
-				}*/
-				//console.log(booklist);
-/*
-			} else {
-				console.error(xhr.statusText);
-			}
-		}
-	};
-	xhr.onerror = function (e) {
-		console.error(xhr.statusText);
-	};
-	xhr.send(null);
-}
-*/
